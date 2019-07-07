646ba01da10923626b57f08edda38024bbeeff6c
Larbi Gharib <larbizard@gmail.com>
2019-07-07 14:55:35 -0400

    [ADD] Added method to get occupancy

--- getOccupation.py
+++ getOccupation.py
@@ -1,15 +1,5 @@
 from BixiStationOccupation import BixiStationOccupation
 
-import http.server
-import socketserver
-
-PORT = 8000
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
 
 def run():
     day = 'W-WED'
